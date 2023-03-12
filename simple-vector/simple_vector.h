10f75b708a68faad638f42824c8265b82a7a1487
Jack-Archer <114923937+Jack-Archer@users.noreply.github.com>
2023-03-12 16:23:34 +0300

    Update simple_vector.h

--- simple-vector/simple_vector.h
+++ simple-vector/simple_vector.h
@@ -1,11 +1,10 @@
-#pragma once
-
 #include "array_ptr.h"
 #include <stdexcept>
 #include <algorithm>
 #include <initializer_list>
 #include <iostream>
 #include  <utility>
+#include <cassert>
 
 class ReserveProxyObj {
 public:    
@@ -35,45 +34,30 @@ public:
 
     SimpleVector() noexcept = default;
 
-    explicit SimpleVector(size_t size)
-        : elements_(size)
-    {
-        size_ = size;
-        capacity_ = size;
+    explicit SimpleVector(size_t size) : elements_(size), size_(size), capacity_(size) {
         for(auto it = begin(); it != end(); ++it) {
             *it = Type();
         }
     }
     
-    SimpleVector(size_t size, const Type& value)
-        : elements_(size)
-    {
-        size_ = size;
-        capacity_ = size; 
-        for(auto it = begin(); it != end(); ++it) {
-            *it = value;
-        }
+    SimpleVector(size_t size, const Type& value) : elements_(size), size_(size), capacity_(size) {
+       std::fill(begin(), end(), value);
     }
 
-   SimpleVector(std::initializer_list<Type> init)
-        : elements_(init.size())
-    {
-        size_ = init.size();
-        capacity_ = init.size();
+   SimpleVector(std::initializer_list<Type> init) : elements_(init.size()), size_(init.size()),  capacity_ (init.size()) {
         std::copy(init.begin(), init.end(), begin());
     }
     
     
-    SimpleVector(const SimpleVector& other) : elements_(other.size_) {
-        size_ = other.size_;
-        capacity_ = other.size_;
+    SimpleVector(const SimpleVector& other) : elements_(other.size_), size_(other.size_), capacity_(other.size_) {
         std::copy(other.begin(), other.end(), begin());
     }
     
     
     SimpleVector& operator=(const SimpleVector& rhs) {
-        if(&rhs == this)
+        if(&rhs == this) {
             return *this;
+        }
         SimpleVector<Type> temp(rhs);
         swap(temp);
         return *this;
@@ -115,13 +99,12 @@ public:
     }
 
     const Type& operator[](size_t index) const noexcept {
+        assert(index > size_);
         return elements_[index];
     }
 
     Type& At(size_t index) {
-        if (index >= GetSize()) {
-            throw std::out_of_range("Error: incorrect index");
-        }
+        assert(index > size_);
         return elements_[index];
     }
 
@@ -190,66 +173,73 @@ public:
     
     
     Iterator Insert(ConstIterator pos, const Type& value) {
-        auto dist = std::distance(cbegin(), pos);
-        if(dist < 0 || dist > (long int)size_) {
-            throw std::out_of_range("Error: incorrect pos");
-        }
-        if (IsEmpty()){
-            PushBack(value);
-            return begin();
-        }
-        
-        if (size_ == capacity_) {
-            SimpleVector<Type> temp(size_ << 1);
-            std::copy(begin(), begin() + dist, temp.begin());
-            temp[dist] = value;
-            std::copy(begin() + (dist), end(), temp.begin() + (dist + 1));
-            temp.size_ = size_ + 1;
-            swap(temp);
+        if (pos >= begin() && pos < end()) {
+            auto dist = std::distance(cbegin(), pos);
+            if(dist < 0 || dist > (long int)size_) {
+                throw std::out_of_range("Error: incorrect pos");
+            }
+            if (IsEmpty()){
+                PushBack(value);
+                return begin();
+            }
+
+            if (size_ == capacity_) {
+                SimpleVector<Type> temp(size_ << 1);
+                std::copy(begin(), begin() + dist, temp.begin());
+                temp[dist] = value;
+                std::copy(begin() + (dist), end(), temp.begin() + (dist + 1));
+                temp.size_ = size_ + 1;
+                swap(temp);
+                return (begin() + dist);
+            }
+
+            std::copy_backward((Iterator)pos, end(), end() + 1);
+            elements_[dist] = value;
+            ++size_;
             return (begin() + dist);
         }
-        
-        std::copy_backward((Iterator)pos, end(), end() + 1);
-        elements_[dist] = value;
-        ++size_;
-        return (begin() + dist);
+        return end();
     }
     
     Iterator Insert(ConstIterator pos, Type&& value) {
-        auto dist = std::distance(cbegin(), pos);
-        if(dist < 0 || dist > (long int)size_) {
-            throw std::out_of_range("Error: incorrect pos");
-        }
-        if (IsEmpty()){
-            PushBack(std::move(value));
-            return begin();
-        }
-        
-        if (size_ == capacity_) {
-            SimpleVector<Type> temp(size_ << 1);
-            std::move(begin(), begin() + dist, temp.begin());
-            temp[dist] = std::move(value);
-            std::move(begin() + (dist), end(), temp.begin() + (dist + 1));
-            temp.size_ = size_ + 1;
-            swap(temp);
+            auto dist = std::distance(cbegin(), pos);
+            if(dist < 0 || dist > (long int)size_) {
+                throw std::out_of_range("Error: incorrect pos");
+            }
+            if (IsEmpty()){
+                PushBack(std::move(value));
+                return begin();
+            }
+
+            if (size_ == capacity_) {
+                SimpleVector<Type> temp(size_ << 1);
+                std::move(begin(), begin() + dist, temp.begin());
+                temp[dist] = std::move(value);
+                std::move(begin() + (dist), end(), temp.begin() + (dist + 1));
+                temp.size_ = size_ + 1;
+                swap(temp);
+                return (begin() + dist);
+            }
+
+            std::move_backward((Iterator)pos, end(), end() + 1);
+            elements_[dist] = std::move(value);
+            ++size_;
             return (begin() + dist);
-        }
-        
-        std::move_backward((Iterator)pos, end(), end() + 1);
-        elements_[dist] = std::move(value);
-        ++size_;
-        return (begin() + dist);
     }
     
     
     void PopBack() noexcept {
-       if (!IsEmpty()) {--size_;}
+        assert(IsEmpty());
+        --size_;
     }
     
     Iterator Erase(ConstIterator pos) {
+    if (pos >= begin() && pos < end()) {
         std::move((Iterator)pos + 1, end(), (Iterator)pos);
         --size_;
         return (Iterator)pos;
+    }
+    return end();
     }
     
     void swap(SimpleVector& other) noexcept {
@@ -274,12 +264,12 @@ public:
 
 
     Iterator begin() noexcept {
-        return &elements_[0];
+        return elements_.Get();
     }
 
 
     Iterator end() noexcept {
-        return &elements_[size_];
+        return (elements_.Get() + size_);
     }
 
     ConstIterator begin() const noexcept {
